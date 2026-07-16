e70cea936c6f6152b94b7ab9b5d15c5e5432e871
Amar <your-email@example.com>
2026-07-17 03:29:37 +0530

    Read DEBUG from environment variable instead of hardcoding

--- sentora_core/settings.py
+++ sentora_core/settings.py
@@ -12,7 +12,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
